f232ba5da9115ba4a8fe100c502ca5b467f95d81
Vetal Lamkov <skifvetal@gmail.com>
2019-09-20 17:02:13 +0400

    Init commit

---
+++ mealorderbot/src/main/java/com/mealorderbot/entites/Product.java
@@ -0,0 +1,83 @@
+package com.mealorderbot.entites;
+import java.sql.Date;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "PRODUCT_ID")
+	private int	productId;
+	@Column(name = "DATE_PRODUCT")
+	private String dateProduct;
+	@Column(name = "NAME")
+	private String name;
+	@Column(name = "PRICE")
+	private Double price;
+	@Column(name = "NOMENCLATURE")
+	private String nomenclature;
+	
+	public Product() {
+		// TODO Auto-generated constructor stub
+	}
+	public Product( String dateProduct, String name, Double price, String nomenclature) {
+		
+//		setProductId(productId);
+		setDateProduct(dateProduct);
+		setName(name);
+		setPrice(price);
+		setNomenclature(nomenclature);
+	}
+	
+	public int getProductId() {
+		return productId;
+	}
+
+
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getDateProduct() {
+		return dateProduct;
+	}
+
+	public void setDateProduct(String dateProduct) {
+		this.dateProduct = dateProduct;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getNomenclature() {
+		return nomenclature;
+	}
+
+	public void setNomenclature(String nomenclature) {
+		this.nomenclature = nomenclature;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", dateProduct=" + dateProduct + ", name=" + name + ", price="
+				+ price + ", nomenclature=" + nomenclature + "]";
+	}
+	
+
+
+}
